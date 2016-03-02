68d375ea1d479713eba9e49ca559ab09cd056a74
torsinclair <tor@romanesco.se>
2016-03-02 15:48:35 -0800

    Reports a size of zero for a new stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,45 @@
+
+
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.size = size;
+
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var size = function() {
+  return 0;
+};
+
+// var stackMethods = {};
+
+// var Stack = function() {
+//   var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var items = 0;
+
+
+  // Implement the methods below
+  // var push = function(value) {
+
+  //   items++;
+  //   return storage[items] = value;
+
+  // };
+
+  // var pop = function() {
+  //   var val = storage[items];
+  //   delete storage[items];
+  //   items = Math.max(0, --items);
+  //   return val;
+
+  // };
+
 
 
