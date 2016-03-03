1fe868b3cd76a448a878acf4acc0f69171f33e18
torsinclair <tor@romanesco.se>
2016-03-02 20:20:46 -0800

    Complete queue shared behaviour, queue-specific behaviour

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[items] = value;
+    items++;
   };
 
   someInstance.dequeue = function() {
+    var value = storage[0];
+    delete storage[0];
+    items = Math.max(0, --items);
+
+    _.reduce(storage, function(acc, item, key){
+      acc[key-1] = item;
+    }, storage);
+
+    return value;
   };
 
   someInstance.size = function() {
+    return items;
+
   };
 
   return someInstance;
+
 };
