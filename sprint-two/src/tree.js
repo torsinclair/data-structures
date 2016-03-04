975708bb91bd96c6502f07c47880a7c869187b42
torsinclair <tor@romanesco.se>
2016-03-03 16:31:01 -0800

    Will be able to add children to a tree's child

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+   
+  _.extend(newTree, treeMethods);
+  
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,11 +12,36 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = new Tree(value);
+
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  
+  //search current node for target
+  var doesContain = false;
+  //search children  
+  for(var i = 0; i < this.children.length; i++){
+    if (this.children[i].value === target){
+      doesContain = true;
+    }  
+  }
+
+  return doesContain;
 };
 
+
+
+/*var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  //node.next = null;
+
+  return node;
+};
+*/
 
 
 /*
