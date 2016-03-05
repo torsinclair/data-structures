ff3379d50226b6f9671265ab5cc2c69715735d2c
torsinclair <tor@romanesco.se>
2016-03-04 21:48:37 -0800

    Handles hash function collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,49 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(key, value) {
   var index = getIndexBelowMaxForKey(key, this._limit);
-  this._storage.set(index, value);
+  
+  var valArr = this._storage.get(index);  //this.retrieve(key);
+  var found = false;
+
+  if (valArr === undefined) {
+    this._storage.set(index, [[key, value]]);
+  } else {
+    //search valArr for value
+    for (var i = 0; i < valArr.length; i++) {
+      if (valArr[i][0] === key) {
+        valArr[i][1] = value;
+        found = true;
+      }
+    }
+
+    if (!found) {
+      valArr.push([key, value]);
+    }
+
+    this._storage.set(index, valArr);
+
+    valArr.push(value); 
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(key) {
   var index = getIndexBelowMaxForKey(key, this._limit);
-  return this._storage.get(index);
+  var valArr = this._storage.get(index);
+
+  if (!valArr) {
+    return;
+  }
+
+  var value;
+
+  for (var i = 0; i < valArr.length; i++) {
+    if (valArr[i][0] === key) {
+      value = valArr[i][1];
+    }
+  }
+  
+  return value;
 };
 
 HashTable.prototype.remove = function(key) {
