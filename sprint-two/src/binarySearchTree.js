6dc87efeb339a8b69dee87bed9cf12216cba1fa0
torsinclair <tor@romanesco.se>
2016-03-05 10:57:31 -0800

    Insert values in the correct location using the insert function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,6 +4,7 @@ var BinarySearchTree = function(value) {
 
   tree.left;
   tree.right;
+  tree.value = value;
 
   return tree;
 
@@ -11,7 +12,31 @@ var BinarySearchTree = function(value) {
 
 var binaryTreeMethods = {
   insert: function(value) {
-  
+
+    // create new tree object
+    var newTree = BinarySearchTree(value);
+    // test original value against new tree's value
+    // if new value is less
+    if (this.value > value) {
+      // put new tree in right
+      // test if right already exists
+      if (this.left) {
+       // if it does call insert on child node
+        this.left.insert(value);
+      } else {
+        // it doesn't already exist, set to right
+        this.left = newTree;
+      }
+    } else {
+      // if new value is greater than current value
+      if (this.right) {
+       // if it does exist, call insert on child node
+        this.right.insert(value);
+      } else {
+        // it doesn't already exist, set to right
+        this.right = newTree;
+      }
+    }
   },
   contains: function(value) {
     return false;
