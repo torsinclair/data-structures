0fcc59114b72aec0b1d861d2ae476784f4e3f15a
torsinclair <tor@romanesco.se>
2016-03-05 10:11:43 -0800

    Define insert contains and depthFirstLog functions

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,24 @@
 var BinarySearchTree = function(value) {
+
+  var tree = Object.create(binaryTreeMethods);
+
+  tree.left;
+  tree.right;
+
+  return tree;
+
+};
+
+var binaryTreeMethods = {
+  insert: function(value) {
+  
+  },
+  contains: function(value) {
+    return false;
+  },
+  depthFirstLog: function(callback) {
+    // execute callback on every value in tree
+  }
 };
 
 
