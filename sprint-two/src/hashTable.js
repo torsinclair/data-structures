efa259a6ed84cc9ebd547f95665655b1cce4a8dd
torsinclair <tor@romanesco.se>
2016-03-04 20:14:29 -0800

    Will not contain values that were removed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,19 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.set(index, value);
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  return this._storage.get(index);
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
