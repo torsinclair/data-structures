e34bd6f4b029e1257827eacebcaf5d2074b9c34c
Tor Sinclair <tor@romanesco.se>
2016-03-06 05:28:28 -0800

    Pass queue functional instantiation style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    items++;
+    storage[items] = value;
   };
 
   someInstance.dequeue = function() {
+    if(items !== 0){
+    var val = storage[1];
+    delete storage[1];  
+    items--;
+    return val;
+    }  
   };
 
   someInstance.size = function() {
+    return items;
   };
 
   return someInstance;
