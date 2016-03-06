a46e12fb06a8a13fa1cf84f98aebbb035c5968e1
Tor Sinclair <tor@romanesco.se>
2016-03-06 10:23:21 -0800

    Pass all tests except keyword this error

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+};
+
+Stack.prototype.storage = {};
+Stack.prototype.items = 0;
+
+Stack.prototype.size = function(){
+  return this.items;
+};
+
+Stack.prototype.push = function(value){
+  this.items++;
+  this.storage[this.items] = value;
+
+};
+
+Stack.prototype.pop = function(){
+  if(this.items !== 0){
+    var val = this.storage[this.items];
+    delete this.storage[this.items];
+    this.items--;
+    return val;
+  }
+
 };
 
 
