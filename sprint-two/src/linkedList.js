a77a17a8faac92f5a80c3dba672414a5b6cc68d6
Tor Sinclair <tor@romanesco.se>
2016-03-06 12:43:22 -0800

    Designate a new tail when new nodes are added

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // are we initializing an empty container?
+    if(list.head === null){
+      // Yes
+      list.head = Node(value);
+      list.tail = list.head;
+    }else{
+      // No, there are objects
+        // OK, what happens then?
+          // nest the new object in the current tail
+          list.tail.next = Node(value);
+          list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if(list.head !== null){
+      var oldHead = list.head;
+      delete list.head;
+      list.head = list.head.next;
+    }
   };
 
   list.contains = function(target) {
