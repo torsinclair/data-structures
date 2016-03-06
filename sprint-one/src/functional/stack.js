eba517cf90ea0cea8407e569a5a3c425a987cd10
Tor Sinclair <tor@romanesco.se>
2016-03-05 20:43:01 -0800

    Complete no error on removing from an empty stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    items++;
+    storage[items] = value;
   };
 
   someInstance.pop = function() {
+    if(items > 0){
+      delete storage[items];
+      items--;
+    }
+    
   };
 
   someInstance.size = function() {
+    return items;
   };
 
   return someInstance;
 };
+
