687744ac74c74b5044e272a16e16f5c4e9c67df0
Tor Sinclair <tor@romanesco.se>
2016-03-06 10:38:32 -0800

    Pass queue shared behaviour tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,4 +3,28 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Queue.prototype.items = 0;
+
+Queue.prototype.size = function(){
+  return this.items;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.items++;
+  this[this.items] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.items !== 0){
+    var val = this['1'];
+    delete this['1'];
+    this.items--;
+
+    _.reduce(this, function(acc, value, key){
+      acc = this[key-1] = value;
+    },this);
+  return val;
+  }
+};
+
 
