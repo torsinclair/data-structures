46c3171bacf27bd5f178142bf71083f403f9dbad
Tor Sinclair <tor@romanesco.se>
2016-03-06 16:53:43 -0800

    Pass all tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree, treeMethods);
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +13,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+
+  if(this.value === target){
+    return true;
+  } 
+    
+  for (var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target)){
+      return true;
+    }
+  }
+  
+
+  return false;
 };
 
 
